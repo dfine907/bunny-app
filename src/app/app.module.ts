21b6150e94461570265968879574d63a26c4ca34
Dina <dinagiacobbi@gmail.com>
2024-03-12 14:42:52 -0800

    Editing components adding borders for diagnostics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -11,7 +11,6 @@ import { MoreInfoComponent } from './more-info/more-info.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     HomeComponent,
     BunnyProfileComponent,
     MoreInfoComponent
