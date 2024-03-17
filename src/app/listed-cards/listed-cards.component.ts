3a1c6918e9b8a22cdcf76ceda18dad87e6ee7bfc
Dina <dinagiacobbi@gmail.com>
2024-03-17 06:15:22 -0800

    Modifying display cards and form

--- src/app/listed-cards/listed-cards.component.ts
+++ src/app/listed-cards/listed-cards.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Bunny } from '../bunny';
+
 @Component({
   selector: 'app-listed-cards',
   templateUrl: './listed-cards.component.html',
   styleUrls: ['./listed-cards.component.css']
 })
-export class ListedCardsComponent {
-  
 
+export class ListedCardsComponent {
+  bunnyId = 10;
+  bunnyStatus = "not added"
+
+  getBunStatus() {
+    return this.bunnyStatus
+  }
 }
